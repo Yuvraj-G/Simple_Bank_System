87b1a96e15d9b12e7c7e5f435f807801f633af3b
Yuvraj-G <cubehood675@gmail.com>
2023-12-27 21:58:22 +0530

    SQL Commands added

--- details.py
+++ details.py
@@ -2,3 +2,26 @@ user = "root"
 host = "localhost"
 password = "Yuvraj675"
 database = "bank"
+
+#SQL Commands
+
+
+# CREATE TABLE accounts (
+#     Aadhar_No BIGINT NOT NULL PRIMARY KEY,
+#     Account_No BIGINT UNIQUE NOT NULL,
+#     Account_Type VARCHAR(7) NOT NULL,
+#     Balance FLOAT NOT NULL,
+#     Pin INT NOT NULL);
+
+# CREATE TABLE personal (
+#     Aadhar_No BIGINT NOT NULL PRIMARY KEY,
+#     Name VARCHAR(255) NOT NULL,
+#     Age INT NOT NULL,
+#     Gender VARCHAR(1) NOT NULL);
+
+# CREATE TABLE transactions (
+#     Date DATE NOT NULL,
+#     Account_No BIGINT NOT NULL,
+#     Transaction_Type VARCHAR(10) NOT NULL,
+#     Amount INT NOT NULL,
+#     FOREIGN KEY (Account_No) REFERENCES accounts(Account_No));
